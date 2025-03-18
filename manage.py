0d3dcb0bd44435ae5d02e15bed14b1cf3a2381b0
C-MORE-2005 <chinmayeemore003@gmail.com>
2025-03-18 19:55:16 +0530

    Updated code

--- manage.py
+++ manage.py
@@ -24,3 +24,4 @@ if __name__ == '__main__':
 #hiiiiiiiiiiiiiiiiiii heeeeeeeeeeeeeeeeeenaaaaaaaaaaa
 #alelelele
 #stop doing alelelelelelel
+#iam done withuuuuuuuuuuuuuuu=hghjhgfgshjdfhgvbsdhjchgvcsdvchjb
